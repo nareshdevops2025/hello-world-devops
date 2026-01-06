296b15380ebcaecdc04450c3c1e0c604002c20e5
nareshdevops2025 <naresh.devops2025@gmail.com>
2026-01-06 11:14:14 +0530

    Update HelloApplication.java

--- app/src/main/java/com/example/hello/HelloApplication.java
+++ app/src/main/java/com/example/hello/HelloApplication.java
@@ -7,5 +7,6 @@ public class HelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+        System.out.println("Hello, Naresh");
     }
 }
